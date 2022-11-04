80876defeb6d5524646f07790861ce85958bfaee
Nikhil kumar Singh <113193191+NikhilSingh97@users.noreply.github.com>
2022-11-04 15:14:53 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,57 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Contact Book</title>
+<!-- CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+
+
+<div class="row justify-content-center wrapper ">
+<div class="col-md-4 pt-4 rounded-3">
+<div class="card rounded-4">
+<header class="card-header">
+	<h4 class="card-title mt-2 text-center">Log In</h4>
+</header>
+<article class="card-body">
+<form method="POST" action="">
+	<div class="form-group">
+		<label>Email</label>
+		<input type="email" name="email" class="form-control" placeholder="Email">
+	</div> 
+	<div class="form-group">
+		<label>Password</label>
+	    <input class="form-control" type="password" name="password" placeholder="password">
+	</div>   
+    <div class="form-group">
+        <button type="submit" name="submit" class="btn btn-success btn-block"> Login  </button>
+    </div> 
+    
+    <div class="form-group col-12">
+    <div class="row justify-content-center">
+        <div class="col-5 text-left"><a href="">Sign Up</a></div>
+        <div class="col-5 text-right"><a href="">Forgot Password</a></div>
+        
+    </div>
+    </div>
+</div>      
+</form>
+</article>
+
+</div>
+</div>
+
+</div>
+
+</main>
+
+</body>
+</html>
